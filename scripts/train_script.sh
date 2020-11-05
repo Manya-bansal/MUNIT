a4d91d6a5c6c756ac9f47fff62c17294898fa4c6
Ubuntu <ubuntu@ip-172-31-46-114.ec2.internal>
2020-11-05 17:08:28 +0000

    updated datasets, updated training configs and scripts

--- scripts/train_script.sh
+++ scripts/train_script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python train.py --config configs/custom_config.yaml
+python train.py --config configs/custom_config_canny.yaml --output_path ./canny_outputs
+python train.py --config configs/custom_config_HED.yaml --output_path ./HED_outputs
