b300725f99958d1502b83668c330dbaad42196eb
Ubuntu <ubuntu@ip-172-31-46-114.ec2.internal>
2020-11-05 15:58:31 +0000

    modified config and testing script

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python test.py --config configs/custom_config.yaml --input inputs/edges2shoes_edge.jpg --output_folder results/edges2shoes --checkpoint models/edges2shoes.pt --a2b 1
+python test.py --config configs/custom_config.yaml --input inputs/edges2shoes_edge.jpg --output_folder results/testing_canny --checkpoint models/canny.pt --a2b 1
